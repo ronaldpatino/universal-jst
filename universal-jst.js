bc16ad28695672e72f540a98b76c302fb23906f7
Ronald Patino <ronaldpatino@gmail.com>
2012-03-21 12:23:15 -0500

    To get it working on windows(d'oh!) changed ../lib to ./lib

--- universal-jst.js
+++ universal-jst.js
@@ -6,7 +6,7 @@ var jst = exports.compile = function jst( inputdir, outputdir, engine ){
 
   var tmpl;
   try{
-    tmpl = require('../lib/' + engine + '-jst');
+    tmpl = require('./lib/' + engine + '-jst');
   }catch(e){
     return console.error('--template ' + engine + ' is not allowed. Use ' + allowedTmpl);
   }
